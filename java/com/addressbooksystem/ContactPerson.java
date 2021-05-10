60b06e7f83ca3cc4ac33582d7791d1c751c3aca2
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-05-10 22:21:07 +0530

    Ability To Negate Duplicate Identity of Same Person

--- java/com/addressbooksystem/ContactPerson.java
+++ java/com/addressbooksystem/ContactPerson.java
@@ -37,10 +37,25 @@ public class ContactPerson {
     public String getEmailId() {
         return emailId;
     }
-    public ContactPerson(String firstName, String lastName, String address) {
+    public ContactPerson(String firstName, String lastName, String address,String city, String state, int zip, long phoneNo, String emailId) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.emailId = emailId;
+    }
+    @Override
+    public boolean equals(Object object) {
+        ContactPerson contactPerson = (ContactPerson)object;
+        String personName = this.firstName+" "+this.lastName;
+        String personName1 = contactPerson.firstName+" "+contactPerson.lastName;
+        int compare = personName.compareTo(personName1);
+        if(compare==0)
+            return true;
+        return false;
     }
         @Override
         public String toString() {
